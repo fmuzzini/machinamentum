d0acbf3032df4ad640282b78b20e5f5ecbda9192
pippomuzzo@gmail.com <pippomuzzo@gmail.com@dcac6b47-b56b-8fad-3876-fb33483ea194>
2014-07-07 12:05:45 +0000

    Finita funzione/i per il salvataggio su file. Corretti alcuni const nei prototipi

--- src/file_IO.h
+++ src/file_IO.h
@@ -3,12 +3,17 @@
 Interfaccia del modulo file_IO
 */
 
-#include <allegro5/allegro.h>
 
 /**
 Salva su file una sequenza di n byte.
 @param[in] cont Puntatore all'inizio della sequenza
-@param[in] n numero di byte da scrivere
-@param[in] file nome da dare al file
+@param[in] n Numero di byte da scrivere
+@param[in] file Nome da dare al file
+@param[in] chiusura Indica se chiudere il file alla fine della scrittura
 */
-bool salva_su_file(const void* cont, int n, const char file[]);
+bool salva_su_file(const void* cont, int n, const char file[], bool chiusrua = false);
+
+/**
+Chiude il file associato all'ultimo stream utilizzato.
+*/
+bool chiudi_file();
